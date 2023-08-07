bf0a9acc28f2b3f3a764f7af612efe26a910b72a
Christian Scott <christian@processfoundry.io>
2023-08-07 12:37:25 -0400

    Removing ddos protection plan.

--- tf/envdna/main.tf
+++ tf/envdna/main.tf
@@ -14,7 +14,6 @@ terraform {
     use_oidc             = true
     subscription_id      = "62c223af-3ea4-4cf8-bb4a-c8449fe872e1"
     tenant_id            = "567e2175-bf4e-4bcc-b114-335fa0061f2f"
-    key                  = "azuresql.tfstate"
   }
 }
 
